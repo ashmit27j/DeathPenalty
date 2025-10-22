dee9fa24a0ccceaf6c2c1af1b74ec44b540ab3b3
Ashmit <ashmit27j@gmail.com>
2025-10-22 19:26:23 +0530

    fix: timer system partially done, checkpoints left

--- Assets/Scripts/Time/StartTimer.cs
+++ Assets/Scripts/Time/StartTimer.cs
@@ -6,6 +6,19 @@ public class StartTimer : MonoBehaviour
     public DeathTimer deathTimer;
     private bool isActivated = false;
 
+    void Start()
+    {
+        if (deathTimer == null)
+        {
+            deathTimer = FindObjectOfType<DeathTimer>();
+
+            if (deathTimer == null)
+            {
+                Debug.LogError("DeathTimer not found in the scene!");
+            }
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !isActivated)
--- Assets/Scripts/Time/StopTimer.cs
+++ Assets/Scripts/Time/StopTimer.cs
@@ -6,8 +6,21 @@ public class StopTimer : MonoBehaviour
     public DeathTimer deathTimer;
     private bool isActivated = false;
 
+    void Start()
+    {
+        if (deathTimer == null)
+        {
+            deathTimer = FindObjectOfType<DeathTimer>();
+            if (deathTimer == null)
+            {
+                Debug.LogError("DeathTimer not found in the scene!");
+            }
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Triggered by: " + other.gameObject.name);
         if (other.CompareTag("Player") && !isActivated)
         {
             isActivated = true;
