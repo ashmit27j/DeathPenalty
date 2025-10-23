e03303d7c46a917a11adc6153c8c3513df53fae6
Ashmit <ashmit27j@gmail.com>
2025-10-23 20:53:32 +0530

    basic shuriken working

---
+++ Assets/Scripts/Enemy/BasicEnemy.cs
@@ -0,0 +1,21 @@
+
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    [Header("Stats")]
+    public int health;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+            DestroyEnemy();
+    }
+
+    public void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+}
